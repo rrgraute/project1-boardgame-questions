656df2c17851d155cf69a120606984f137cb8d0f
Ryan Graute <rrgraute@gmail.com>
2017-09-29 14:34:22 +0200

    add question model, migration and controller

---
+++ database/migrations/2017_09_29_122400_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("question");
+            $table->string("answer_1");
+            $table->string("answer_2");
+            $table->string("answer_3")->nullable();
+            $table->string("answer_4")->nullable();
+            $table->integer("correct_answer");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}

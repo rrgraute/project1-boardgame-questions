d12b5853551904831b7604f8fc6ba3a588e66fdb
Ryan Graute <rrgraute@gmail.com>
2017-10-03 14:34:01 +0200

    added foundation

---
+++ resources/views/questions/question.blade.php
@@ -0,0 +1,9 @@
+@extends('template.base')
+
+@section('content')
+    <div class="row">
+        <div class="small-6 columns">
+            test
+        </div>
+    </div>
+@endsection
---
+++ resources/views/template/base.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+    @yield('content')
+    <script src="/js/app.js"></script>
+</body>
+</html>
